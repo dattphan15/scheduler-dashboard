44f060955b483d3fd1c5d656d5c67397dfd47aa2
Kevin <ken.phan@live.ca>
2021-03-25 13:39:55 -0700

    loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+// import Loading from "scheduler-dashboard/src/components/Loading.js"
 
 import classnames from "classnames";
+ 
 
 class Dashboard extends Component {
+  state = { 
+    loading: true 
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
 }
+
 
 export default Dashboard;
